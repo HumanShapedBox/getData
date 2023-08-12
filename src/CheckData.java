efa160d891be5de7887482bcdffe55783ccfd8f7
HumanBox <stashksenia@gmail.com>
2023-08-12 12:55:13 +0300

    user info

--- src/CheckData.java
+++ src/CheckData.java
@@ -6,21 +6,35 @@ public class CheckData {
 
 
     public void checkMyData(String data){
-        if(checkString(data) == true){
+
+        if(checkString(data)){
             System.out.println(data);
         }else {
-            System.out.println("Oops, something went wrong!");
+            whatsWrong(data);
         }
+    }
 
+    private String[] spliter(String data){
+        String[] splitData = data.split(" ");
+        return splitData;
     }
 
     private boolean checkString(String data){
         if(data == null){return false;}
         boolean flag = true;
-        String[] splitData = data.split(" ");
+        String[] splitData = spliter(data);
         int dataCounter = splitData.length;
         if(counter != dataCounter){flag = false;}
         return flag;
+    }
+
+    private void whatsWrong(String data){
+        int num = data.length();
+        if(num > counter){
+            System.out.println("Вы ввели больше данных, чем требуется");
+        }else{
+            System.out.println("Данных недостаточно");
+        }
     }
 
 
